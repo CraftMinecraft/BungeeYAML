2a81faa8d5c1afb9eb9b4de74cb8dd535e3adfef
Robin Lambertz <robinlambertz.dev@gmail.com>
2013-02-28 13:36:48 +0000

    Turned into a BungeeCord plugin. Fixed pom.xml.

---
+++ src/main/java/net/craftminecraft/bungee/bungeeyaml/BungeeYAML.java
@@ -0,0 +1,13 @@
+package net.craftminecraft.bungee.bungeeyaml;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class BungeeYAML extends Plugin {
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+}
