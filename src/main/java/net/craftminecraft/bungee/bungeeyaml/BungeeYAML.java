3d03f0fcd97eacc00c0736dcdf38c6ccb1955d3f
Robin Lambertz <robinlambertz.dev@gmail.com>
2013-03-31 15:10:57 +0100

    Add metrics.

--- src/main/java/net/craftminecraft/bungee/bungeeyaml/BungeeYAML.java
+++ src/main/java/net/craftminecraft/bungee/bungeeyaml/BungeeYAML.java
@@ -1,11 +1,19 @@
 package net.craftminecraft.bungee.bungeeyaml;
 
 
+import java.io.IOException;
+
 import net.md_5.bungee.api.plugin.Plugin;
 
 public class BungeeYAML extends Plugin {
+	Metrics metrics;
 	public void onEnable() {
-		
+		try {
+			metrics = new Metrics(this);
+			metrics.start();
+		} catch (IOException e) {
+
+		}
 	}
 	
 	public void onDisable() {
